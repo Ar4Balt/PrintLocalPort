09835e76947d688e5c94d584b1c9a9903bd2f43e
Ar4Balt <tunnikovartur@gmail.com>
2025-08-28 00:59:40 +0300

    update file

--- PrintLocalPort_2.bat
+++ PrintLocalPort_2.bat
@@ -1,49 +1,55 @@
 @echo off
+chcp 65001 >nul
+setlocal EnableExtensions EnableDelayedExpansion
 color 71
-title Добавление сетевых портов принтера
+title Управление TCP/IP (Standard) портами принтеров
 
-:: Автоматически получение имени компьютера
-set mypc=%COMPUTERNAME%
+:: путь к встроенному скрипту Windows
+set SCRIPT=%SystemRoot%\System32\Printing_Admin_Scripts\ru-RU\prnport.vbs
+if not exist "%SCRIPT%" (
+    echo Не найден %SCRIPT%
+    pause
+    exit /b
+)
 
 :: ================================
-:: Список доступных сетевых портов
+:: Список портов (имена)
 :: ================================
-set port1=\\%mypc%\KIP7170
-set port2=\\%mypc%\OCE-360 PW360 WPD2
-set port3=\\%mypc%\OCE-360.AZOT.COM.BY_PW360_WPD2
-set port4=\\%mypc%\PRN-OKI-2BCB2D
-set port5=\\%mypc%\PRN-EPS-AED7D2
-set port6=\\%mypc%\PRN-PAN-49509E
-set port7=\\%mypc%\PRN-PAN-4954AE
-set port8=\\%mypc%\PRN-PAN-32DC63
-set port9=\\%mypc%\PRN-PAN-DF6D5A
-set port10=\\%mypc%\PRN-PAN-54BCA6
-set port11=\\%mypc%\PRN-PAN-32DBFA
-set port12=\\%mypc%\PRN-PAN-54BCAD
-set port13=\\%mypc%\PRN-PAN-374302
-set port14=\\%mypc%\PRN-XER-9222E4
-set port15=\\%mypc%\TCS300_1_TCS300_WPD2
-set port16=\\%mypc%\TCS300_2_TCS300 WPD2
-set port17=\\%mypc%\HPOA403D
-set port18=\\%mypc%\HPOA91AD
-set port19=\\%mypc%\HPOA411D
-set port20=\\%mypc%\HP0A411D
-set port21=\\%mypc%\XEROX-6705
+set port1=KIP7170
+set port2=OCE-360 PW360 WPD2
+set port3=OCE-360.AZOT.COM.BY_PW360_WPD2
+set port4=PRN-OKI-2BCB2D
+set port5=PRN-EPS-AED7D2
+set port6=PRN-PAN-49509E
+set port7=PRN-PAN-4954AE
+set port8=PRN-PAN-32DC63
+set port9=PRN-PAN-DF6D5A
+set port10=PRN-PAN-54BCA6
+set port11=PRN-PAN-32DBFA
+set port12=PRN-PAN-54BCAD
+set port13=PRN-PAN-374302
+set port14=PRN-XER-9222E4
+set port15=TCS300_1_TCS300_WPD2
+set port16=TCS300_2_TCS300 WPD2
+set port17=HPOA403D
+set port18=HPOA91AD
+set port19=HPOA411D
+set port20=HP0A411D
+set port21=XEROX-6705
 
 :MENU
 cls
 echo ===========================================
-echo     Управление сетевыми портами принтера
+echo      Управление TCP/IP (Standard) портами
 echo ===========================================
 echo.
-echo ==== Уже существующие порты в системе ====
-REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Ports"
+echo ==== Существующие TCP/IP порты: ====
+cscript //nologo "%SCRIPT%" -l
 echo ===========================================
 echo.
-echo Что вы хотите сделать?
-echo   1) Добавить порты
-echo   2) Удалить порты
-echo   3) Выйти
+echo 1) Добавить порт
+echo 2) Удалить порт
+echo 3) Выход
 echo.
 set /p action=Выберите действие (1-3): 
 
@@ -55,32 +61,28 @@ goto MENU
 :ADD
 cls
 echo ===========================================
-echo   Добавление сетевых портов
+echo           Добавление TCP/IP портов
 echo ===========================================
 echo.
-echo Список доступных портов:
+echo Список доступных имен портов:
 for /L %%i in (1,1,21) do (
     call echo   %%i^) !port%%i!
 )
-echo  22) Ввести свой порт вручную
+echo   22) Ввести своё имя вручную
 echo.
 set /p choice=Введите номера через запятую (например: 1,3,7): 
 set choice=%choice: =%
+set choice=%choice:,= %
 
 for %%i in (%choice%) do (
     if "%%i"=="22" (
-        set /p custom=Введите свой сетевой порт (\\сервер\принтер): 
-        call :AddPort "%custom%"
+        set /p custom=Введите имя хоста/IP: 
+        call :AddPort "!custom!"
     ) else (
         call set "p=%%port%%i%%"
-        call :AddPort "%%p%%"
+        call :AddPort "!p!"
     )
 )
-
-echo.
-echo Перезапускаем службу печати...
-net stop spooler >nul
-net start spooler >nul
 
 pause
 goto MENU
@@ -88,52 +90,52 @@ goto MENU
 :DELETE
 cls
 echo ===========================================
-echo   Удаление сетевых портов
+echo          Удаление TCP/IP портов
 echo ===========================================
 echo.
-echo Список доступных портов:
-for /L %%i in (1,1,21) do (
-    call echo   %%i^) !port%%i!
-)
-echo  22) Указать вручную
+echo Существующие порты:
+cscript //nologo "%SCRIPT%" -l
 echo.
-set /p delchoice=Введите номера через запятую (например: 2,5,10): 
-set delchoice=%delchoice: =%
-
-for %%i in (%delchoice%) do (
-    if "%%i"=="22" (
-        set /p customDel=Введите полный путь порта (\\сервер\принтер): 
-        call :DelPort "%customDel%"
-    ) else (
-        call set "p=%%port%%i%%"
-        call :DelPort "%%p%%"
-    )
-)
-
-echo.
-echo Перезапускаем службу печати...
-net stop spooler >nul
-net start spooler >nul
+set /p delport=Введите имя порта для удаления: 
+call :DelPort "!delport!"
 
 pause
 goto MENU
 
 :AddPort
-REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Ports" /v %~1 >nul 2>&1
+set "PORTNAME=%~1"
+:: Проверка существования
+cscript //nologo "%SCRIPT%" -l | findstr /I "%PORTNAME%" >nul
 if %errorlevel%==0 (
-    echo Порт уже существует: %~1
+    echo Порт уже существует: %PORTNAME%
+    exit /b
+)
+
+echo Добавляем TCP/IP порт: %PORTNAME% ...
+cscript //nologo "%SCRIPT%" -a -r "%PORTNAME%" -h "%PORTNAME%" -o raw -n 9100
+
+if %errorlevel%==0 (
+    echo Порт успешно создан: %PORTNAME%
 ) else (
-    echo Добавляем порт: %~1
-    REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Ports" /v %~1 /t REG_SZ /d "" /f >nul
+    echo Ошибка при создании порта %PORTNAME%
 )
 exit /b
 
 :DelPort
-REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Ports" /v %~1 >nul 2>&1
+set "PORTNAME=%~1"
+:: Проверка существования
+cscript //nologo "%SCRIPT%" -l | findstr /I "%PORTNAME%" >nul
+if %errorlevel%==1 (
+    echo Порт не найден: %PORTNAME%
+    exit /b
+)
+
+echo Удаляем TCP/IP порт: %PORTNAME% ...
+cscript //nologo "%SCRIPT%" -d -r "%PORTNAME%"
+
 if %errorlevel%==0 (
-    echo Удаляем порт: %~1
-    REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Ports" /v "%~1" /f >nul
+    echo Порт успешно удалён: %PORTNAME%
 ) else (
-    echo Порт не найден: %~1
+    echo Ошибка при удалении порта %PORTNAME%
 )
 exit /b
