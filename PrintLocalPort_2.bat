7692238857071b59dad0e25d71ac4f7b635a1624
Ar4Balt <tunnikovartur@gmail.com>
2025-08-28 09:41:41 +0300

    update

--- PrintLocalPort_2.bat
+++ PrintLocalPort_2.bat
@@ -4,7 +4,7 @@ setlocal EnableExtensions EnableDelayedExpansion
 color 71
 title Управление TCP/IP (Standard) портами принтеров
 
-:: путь к встроенному скрипту Windows
+:: ======== Путь к встроенному скрипту Windows ========
 set SCRIPT=%SystemRoot%\System32\Printing_Admin_Scripts\ru-RU\prnport.vbs
 if not exist "%SCRIPT%" (
     echo Не найден %SCRIPT%
@@ -12,30 +12,31 @@ if not exist "%SCRIPT%" (
     exit /b
 )
 
-:: ================================
-:: Список портов (имена)
-:: ================================
-set port1=KIP7170
-set port2=OCE-360 PW360 WPD2
-set port3=OCE-360.AZOT.COM.BY_PW360_WPD2
-set port4=PRN-OKI-2BCB2D
-set port5=PRN-EPS-AED7D2
-set port6=PRN-PAN-49509E
-set port7=PRN-PAN-4954AE
-set port8=PRN-PAN-32DC63
-set port9=PRN-PAN-DF6D5A
-set port10=PRN-PAN-54BCA6
-set port11=PRN-PAN-32DBFA
-set port12=PRN-PAN-54BCAD
-set port13=PRN-PAN-374302
-set port14=PRN-XER-9222E4
-set port15=TCS300_1_TCS300_WPD2
-set port16=TCS300_2_TCS300 WPD2
-set port17=HPOA403D
-set port18=HPOA91AD
-set port19=HPOA411D
-set port20=HP0A411D
-set port21=XEROX-6705
+:: ======== Лог-файл ========
+set LOGFILE=%~dp0PrinterPorts.log
+
+:: ======== Предустановленные порты ========
+set ports[1]=KIP7170
+set ports[2]=OCE-360 PW360 WPD2
+set ports[3]=OCE-360.AZOT.COM.BY_PW360_WPD2
+set ports[4]=PRN-OKI-2BCB2D
+set ports[5]=PRN-EPS-AED7D2
+set ports[6]=PRN-PAN-49509E
+set ports[7]=PRN-PAN-4954AE
+set ports[8]=PRN-PAN-32DC63
+set ports[9]=PRN-PAN-DF6D5A
+set ports[10]=PRN-PAN-54BCA6
+set ports[11]=PRN-PAN-32DBFA
+set ports[12]=PRN-PAN-54BCAD
+set ports[13]=PRN-PAN-374302
+set ports[14]=PRN-XER-9222E4
+set ports[15]=TCS300_1_TCS300_WPD2
+set ports[16]=TCS300_2_TCS300 WPD2
+set ports[17]=HPOA403D
+set ports[18]=HPOA91AD
+set ports[19]=HPOA411D
+set ports[20]=HP0A411D
+set ports[21]=XEROX-6705
 
 :MENU
 cls
@@ -49,13 +50,16 @@ echo ===========================================
 echo.
 echo 1) Добавить порт
 echo 2) Удалить порт
-echo 3) Выход
+echo 3) Экспорт списка портов
+echo 4) Выход
 echo.
-set /p action=Выберите действие (1-3): 
+set /p action=Выберите действие (1-4): 
+echo %date% %time% [MENU] Выбран пункт меню: %action%>>"%LOGFILE%"
 
 if "%action%"=="1" goto ADD
 if "%action%"=="2" goto DELETE
-if "%action%"=="3" exit
+if "%action%"=="3" goto EXPORT
+if "%action%"=="4" exit
 goto MENU
 
 :ADD
@@ -64,26 +68,16 @@ echo ===========================================
 echo           Добавление TCP/IP портов
 echo ===========================================
 echo.
-echo Список доступных имен портов:
+echo Список доступных портов:
 for /L %%i in (1,1,21) do (
-    call echo   %%i^) !port%%i!
+    call echo   %%i^) !ports[%%i]!
 )
 echo   22) Ввести своё имя вручную
 echo.
-set /p choice=Введите номера через запятую (например: 1,3,7): 
+set /p choice=Введите номера через запятую или диапазон (например: 1,3,7-9): 
+echo %date% %time% [ADD] Введено: %choice%>>"%LOGFILE%"
 set choice=%choice: =%
-set choice=%choice:,= %
-
-for %%i in (%choice%) do (
-    if "%%i"=="22" (
-        set /p custom=Введите имя хоста/IP: 
-        call :AddPort "!custom!"
-    ) else (
-        call set "p=%%port%%i%%"
-        call :AddPort "!p!"
-    )
-)
-
+call :ParseChoice "%choice%"
 pause
 goto MENU
 
@@ -97,45 +91,128 @@ echo Существующие порты:
 cscript //nologo "%SCRIPT%" -l
 echo.
 set /p delport=Введите имя порта для удаления: 
-call :DelPort "!delport!"
-
+echo %date% %time% [DELETE] Выбран порт для удаления: %delport%>>"%LOGFILE%"
+set /p confirm=Вы уверены, что хотите удалить порт "%delport%"? (Y/N): 
+echo %date% %time% [DELETE] Подтверждение: %confirm%>>"%LOGFILE%"
+if /I "%confirm%"=="Y" (
+    call :DelPort "%delport%"
+) else (
+    echo Отмена удаления.
+    echo %date% %time% [DELETE] Удаление отменено>>"%LOGFILE%"
+)
 pause
 goto MENU
 
+:EXPORT
+cls
+echo ===========================================
+echo       Экспорт списка TCP/IP портов
+echo ===========================================
+cscript //nologo "%SCRIPT%" -l > "%~dp0PrinterPortsList.txt"
+echo Список портов сохранен в PrinterPortsList.txt
+echo %date% %time% [EXPORT] Список портов сохранён>>"%LOGFILE%"
+pause
+goto MENU
+
+:ParseChoice
+set "input=%~1"
+set "input=%input:,= %"  :: заменяем запятые на пробелы
+
+for %%a in (%input%) do (
+    set "element=%%a"
+    set "element=!element: =!"
+    :: Проверяем, есть ли дефис
+    echo !element! | findstr "-" >nul
+    if !errorlevel! == 0 (
+        :: Разбираем диапазон
+        for /F "tokens=1,2 delims=-" %%b in ("!element!") do (
+            set /A start=%%b
+            set /A end=%%c
+            if "!end!"=="" set /A end=!start!
+            for /L %%i in (!start!,1,!end!) do (
+                call set "portname=%%ports[%%i]%%"
+                if "!portname!"=="" (
+                    echo [Ошибка] Порт %%i не существует, пропуск.
+                    echo %date% %time% [ERROR] Порт %%i не существует, пропуск>>"%LOGFILE%"
+                ) else (
+                    echo %date% %time% [ADD] Добавление порта: !portname!>>"%LOGFILE%"
+                    call :AddPort "!portname!"
+                )
+            )
+        )
+    ) else (
+        :: Одиночный номер или пользовательский ввод
+        if "!element!"=="22" (
+            set /p custom=Введите имя хоста/IP: 
+            if "!custom!"=="" (
+                echo [Ошибка] Пустое имя порта, пропуск.
+                echo %date% %time% [ERROR] Пустое имя порта, пропуск>>"%LOGFILE%"
+            ) else (
+                echo %date% %time% [ADD] Пользовательский порт: !custom!>>"%LOGFILE%"
+                call :AddPort "!custom!"
+            )
+        ) else (
+            call set "portname=%%ports[%%a]%%"
+            if "!portname!"=="" (
+                echo [Ошибка] Порт %%a не существует, пропуск.
+                echo %date% %time% [ERROR] Порт %%a не существует, пропуск>>"%LOGFILE%"
+            ) else (
+                echo %date% %time% [ADD] Добавление выбранного порта: !portname!>>"%LOGFILE%"
+                call :AddPort "!portname!"
+            )
+        )
+    )
+)
+goto :eof
+
 :AddPort
 set "PORTNAME=%~1"
-:: Проверка существования
-cscript //nologo "%SCRIPT%" -l | findstr /I "%PORTNAME%" >nul
-if %errorlevel%==0 (
-    echo Порт уже существует: %PORTNAME%
+if "%PORTNAME%"=="" (
+    echo [Ошибка] Пустое имя порта, пропуск.
+    echo %date% %time% [ERROR] Пустое имя порта>>"%LOGFILE%"
     exit /b
 )
-
+cscript //nologo "%SCRIPT%" -l | findstr /I "%PORTNAME%" >nul
+if %errorlevel%==0 (
+    echo [Ошибка] Порт уже существует: %PORTNAME%
+    echo %date% %time% [ERROR] Порт уже существует: %PORTNAME%>>"%LOGFILE%"
+    exit /b
+)
 echo Добавляем TCP/IP порт: %PORTNAME% ...
 cscript //nologo "%SCRIPT%" -a -r "%PORTNAME%" -h "%PORTNAME%" -o raw -n 9100
-
 if %errorlevel%==0 (
-    echo Порт успешно создан: %PORTNAME%
+    echo [OK] Порт успешно создан: %PORTNAME%
+    echo %date% %time% [OK] Порт успешно создан: %PORTNAME%>>"%LOGFILE%"
 ) else (
-    echo Ошибка при создании порта %PORTNAME%
+    echo [Ошибка] Ошибка при создании порта %PORTNAME%
+    echo %date% %time% [ERROR] Ошибка при создании порта %PORTNAME%>>"%LOGFILE%"
 )
 exit /b
 
 :DelPort
 set "PORTNAME=%~1"
-:: Проверка существования
-cscript //nologo "%SCRIPT%" -l | findstr /I "%PORTNAME%" >nul
-if %errorlevel%==1 (
-    echo Порт не найден: %PORTNAME%
+if "%PORTNAME%"=="" (
+    echo [Ошибка] Имя порта не указано!
+    echo %date% %time% [ERROR] Имя порта не указано>>"%LOGFILE%"
     exit /b
 )
-
-echo Удаляем TCP/IP порт: %PORTNAME% ...
-cscript //nologo "%SCRIPT%" -d -r "%PORTNAME%"
-
+echo Удаляем TCP/IP порт: %PORTNAME%
+echo %date% %time% [DELETE] Удаляем порт: %PORTNAME%>>"%LOGFILE%"
+cscript //nologo "%SCRIPT%" -l | findstr /I "%PORTNAME%" >nul
+if %errorlevel%==1 (
+    echo [Ошибка] Порт не найден: %PORTNAME%
+    echo %date% %time% [ERROR] Порт не найден: %PORTNAME%>>"%LOGFILE%"
+    exit /b
+)
+for /f "delims=" %%o in ('cscript //nologo "%SCRIPT%" -d -r "%PORTNAME%" 2^>^&1') do (
+    echo %%o
+    echo %date% %time% [DELETE_OUTPUT] %%o>>"%LOGFILE%"
+)
 if %errorlevel%==0 (
-    echo Порт успешно удалён: %PORTNAME%
+    echo [OK] Порт успешно удалён: %PORTNAME%
+    echo %date% %time% [OK] Порт успешно удалён: %PORTNAME%>>"%LOGFILE%"
 ) else (
-    echo Ошибка при удалении порта %PORTNAME%
+    echo [Ошибка] Не удалось удалить порт %PORTNAME% (код ошибки: %errorlevel%)
+    echo %date% %time% [ERROR] Не удалось удалить порт %PORTNAME% (код ошибки: %errorlevel%)>>"%LOGFILE%"
 )
 exit /b
